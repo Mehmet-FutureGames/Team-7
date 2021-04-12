63950a31ad888861b974ee9b9a0799dad4a1db81
Gabinus <gabinus_93@hotmail.com>
2021-04-12 15:38:27 +0200

    Added different move patterns

--- Assets/MehmetScripts/Scripts/EnemyScripts/Enemy.cs
+++ Assets/MehmetScripts/Scripts/EnemyScripts/Enemy.cs
@@ -12,11 +12,13 @@ public class Enemy : MonoBehaviour
     [Space]
     float movementSpeed;
     float attackDamage;
-
+    int health;
+    float moveDistance;
     GameObject agentObj;
     NavMeshAgent agent;
     Transform parent;
-
+    private MovePattern movePattern;
+    int num = 0;
     Transform player;
 
     [SerializeField] EnemyStats stats;
@@ -26,8 +28,8 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         enemyName = stats.enemyName;
-
         movementSpeed = stats.movementSpeed;
+        moveDistance = stats.moveDistance;
         attackDamage = stats.attackDamage;
 
         parent = GetComponent<Transform>();
@@ -36,41 +38,109 @@ public class Enemy : MonoBehaviour
         
         agent = GetComponentInChildren<NavMeshAgent>();
 
-        notePublisher = FindObjectOfType<NotePublisher>();
+        
 
         player = FindObjectOfType<MovePlayer>().transform;
 
-        notePublisher.noteHit += EnemyMove;
-        notePublisher.noteNotHit += EnemyMove;
-
         Debug.Log("Fear not " + enemyName + " is here");
         //player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    private void OnEnable()
+    {
+        movePattern = stats.movePattern;
+        notePublisher = FindObjectOfType<NotePublisher>();
+        SetMovePattern();
+    }
+    private void OnDisable()
+    {
+        RemoveMovePattern();
     }
 
     public void EnemyMove()
     {
         Vector3 dir = (player.position - agentObj.transform.position).normalized;
         float distance = (player.position - agentObj.transform.position).magnitude;
-        Debug.Log(dir);
-        float walkDistance = 8f;
-        if(distance < walkDistance +1 )
+        
+        if(distance < moveDistance +1 )
         {
             agent.SetDestination(player.position);
         }
         else
         {
-            agent.SetDestination(agentObj.transform.position + dir * walkDistance);
+            agent.SetDestination(agentObj.transform.position + dir * moveDistance);
         }
-        
-        
         if (agent.velocity.sqrMagnitude > Mathf.Epsilon)
         {
             agentObj.transform.rotation = Quaternion.LookRotation(dir);
         }
         
     }
+    public void EnemyMoveEveryOther()
+    {
+        
+        if (num == 0)
+        {
+            Vector3 dir = (player.position - agentObj.transform.position).normalized;
+            float distance = (player.position - agentObj.transform.position).magnitude;
+
+            if (distance < moveDistance + 1)
+            {
+                agent.SetDestination(player.position);
+            }
+            else
+            {
+                agent.SetDestination(agentObj.transform.position + dir * moveDistance);
+            }
+            if (agent.velocity.sqrMagnitude > Mathf.Epsilon)
+            {
+                agentObj.transform.rotation = Quaternion.LookRotation(dir);
+            }
+            num++;
+        }
+        else { num = 0; }
+
+    }
+
+    void SetMovePattern()
+    {
+        switch (movePattern)
+        {
+            case MovePattern.SimpleMove:
+                notePublisher.noteHit += EnemyMoveEveryOther;
+                notePublisher.noteNotHit += EnemyMoveEveryOther;
+                break;
+            case MovePattern.FastMove:
+                notePublisher.noteHit += EnemyMove;
+                notePublisher.noteNotHit += EnemyMove;
+                break;
+            default:
+                break;
+        }
+    }
+
+    void RemoveMovePattern()
+    {
+        switch (movePattern)
+        {
+            case MovePattern.SimpleMove:
+                notePublisher.noteHit -= EnemyMove;
+                notePublisher.noteNotHit -= EnemyMove;
+                break;
+            case MovePattern.FastMove:
+                notePublisher.noteHit -= EnemyMove;
+                notePublisher.noteNotHit -= EnemyMove;
+                break;
+            default:
+                break;
+        }
+    }
+
     private void EnemyAttack()
     {
 
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
     }
 }
--- Assets/MehmetScripts/Scripts/EnemyScripts/EnemyStats.cs
+++ Assets/MehmetScripts/Scripts/EnemyScripts/EnemyStats.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MovePattern
+{
+    SimpleMove,
+    FastMove
+}
+
 [CreateAssetMenu(fileName = "New EnemyStats", menuName = "ScriptableObjects/EnemyStats", order = 1)]
 public class EnemyStats : ScriptableObject
 {
+    public MovePattern movePattern;
     public GameObject enemyModel;
     public string enemyName;
     [Space]
     public float movementSpeed;
+    public float moveDistance;
     public float attackDamage;
     public float health;
 }
