6cf8dc9fb34f060e9adfec222327bd78c90302ab
Gabinus <gabinus_93@hotmail.com>
2021-04-12 13:05:26 +0200

    Crash Test

--- Assets/MehmetScripts/Scripts/EnemyScripts/Enemy.cs
+++ Assets/MehmetScripts/Scripts/EnemyScripts/Enemy.cs
@@ -13,10 +13,13 @@ public class Enemy : MonoBehaviour
     float attackDamage;
 
     NavMeshAgent agent;
+    Transform parent;
 
     Transform player;
 
     [SerializeField] EnemyStats stats;
+
+    NotePublisher notePublisher;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +27,16 @@ public class Enemy : MonoBehaviour
 
         movementSpeed = stats.movementSpeed;
         attackDamage = stats.attackDamage;
+        parent = GetComponent<Transform>();
+        Instantiate(stats.enemyModel, parent);
+        agent = GetComponentInChildren<NavMeshAgent>();
 
-        agent = GetComponent<NavMeshAgent>();
+        notePublisher = FindObjectOfType<NotePublisher>();
+
+        player = FindObjectOfType<MovePlayer>().transform;
+
+        notePublisher.noteHit += EnemyMove;
+        notePublisher.noteNotHit += EnemyMove;
 
         Debug.Log("Fear not " + enemyName + " is here");
         //player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
@@ -33,11 +44,16 @@ public class Enemy : MonoBehaviour
 
     public void EnemyMove()
     {
-        agent.SetDestination(player.transform.position);
+        Vector3 dir = (player.position - transform.position).normalized;
+        Debug.Log(dir);
+        float walkDistance = 3;
+        agent.SetDestination(transform.position + dir * walkDistance);
+        
         if (agent.velocity.sqrMagnitude > Mathf.Epsilon)
         {
-            transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);
+            transform.rotation = Quaternion.LookRotation(dir);
         }
+        
     }
     private void EnemyAttack()
     {
