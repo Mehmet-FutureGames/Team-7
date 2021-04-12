bbdedee07c11c6df684d56d0d76259d38e79400f
Gabinus <gabinus_93@hotmail.com>
2021-04-12 18:19:57 +0200

    Read description
    added more enemy movement patterns.
    Also made enemyStats scriptableobject more customizable.

--- Assets/MehmetScripts/Scripts/EnemyScripts/Enemy.cs
+++ Assets/MehmetScripts/Scripts/EnemyScripts/Enemy.cs
@@ -9,16 +9,23 @@ public class Enemy : MonoBehaviour
     //SET VARIABLES FROM ENEMYSTATS, DON'T CHANGE THE VARIABLES.
     //THEY GET SET THROUGH THE STATS VARIABLE.
     string enemyName;
-    [Space]
-    float movementSpeed;
-    float attackDamage;
-    int health;
-    float moveDistance;
     GameObject agentObj;
     NavMeshAgent agent;
     Transform parent;
+
+    float movementSpeed;
+    float moveDistance;
+    int notesToMove;
+    float detectionRange;
+
+    float attackDamage;
+    int health;
+
     private MovePattern movePattern;
-    int num = 0;
+    int moveCounter = 0;
+    int attackCounter = 0;
+    
+
     Transform player;
 
     [SerializeField] EnemyStats stats;
@@ -31,7 +38,8 @@ public class Enemy : MonoBehaviour
         movementSpeed = stats.movementSpeed;
         moveDistance = stats.moveDistance;
         attackDamage = stats.attackDamage;
-
+        notesToMove = stats.notesToMove;
+        detectionRange = stats.detectionRange;
         parent = GetComponent<Transform>();
 
         agentObj = Instantiate(stats.enemyModel, parent);
@@ -56,29 +64,38 @@ public class Enemy : MonoBehaviour
         RemoveMovePattern();
     }
 
-    public void EnemyMove()
+    public void EnemyRandomMovement()
     {
-        Vector3 dir = (player.position - agentObj.transform.position).normalized;
-        float distance = (player.position - agentObj.transform.position).magnitude;
-        
-        if(distance < moveDistance +1 )
-        {
-            agent.SetDestination(player.position);
-        }
-        else
-        {
-            agent.SetDestination(agentObj.transform.position + dir * moveDistance);
-        }
+        moveCounter++;
+        Vector3 randomVector = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+        Vector3 agentToRandom = agentObj.transform.position + randomVector;
+        Vector3 dir = (agentToRandom - agentObj.transform.position).normalized;
+        agent.SetDestination(agentObj.transform.position + dir * moveDistance);
+
         if (agent.velocity.sqrMagnitude > Mathf.Epsilon)
         {
             agentObj.transform.rotation = Quaternion.LookRotation(dir);
         }
-        
+        if (moveCounter == notesToMove) { moveCounter = 0; }
     }
-    public void EnemyMoveEveryOther()
+
+    public void EnemyProximityDetection()
     {
-        
-        if (num == 0)
+        float distance = (player.position - agentObj.transform.position).magnitude;
+        if(distance < detectionRange)
+        {
+            EnemyMoveTowardsPlayer();
+        }
+        else
+        {
+            EnemyRandomMovement();
+        }
+    }
+
+    public void EnemyMoveTowardsPlayer()
+    {
+        moveCounter++;
+        if (moveCounter == notesToMove)
         {
             Vector3 dir = (player.position - agentObj.transform.position).normalized;
             float distance = (player.position - agentObj.transform.position).magnitude;
@@ -95,23 +112,27 @@ public class Enemy : MonoBehaviour
             {
                 agentObj.transform.rotation = Quaternion.LookRotation(dir);
             }
-            num++;
+            
         }
-        else { num = 0; }
-
+        if(moveCounter == notesToMove) { moveCounter = 0; }
+        
     }
 
     void SetMovePattern()
     {
         switch (movePattern)
         {
-            case MovePattern.SimpleMove:
-                notePublisher.noteHit += EnemyMoveEveryOther;
-                notePublisher.noteNotHit += EnemyMoveEveryOther;
+            case MovePattern.TowardsPlayer:
+                notePublisher.noteHit += EnemyMoveTowardsPlayer;
+                notePublisher.noteNotHit += EnemyMoveTowardsPlayer;
                 break;
-            case MovePattern.FastMove:
-                notePublisher.noteHit += EnemyMove;
-                notePublisher.noteNotHit += EnemyMove;
+            case MovePattern.RandomDirection:
+                notePublisher.noteHit += EnemyRandomMovement;
+                notePublisher.noteNotHit += EnemyRandomMovement;
+                break;
+            case MovePattern.ProximityDetection:
+                notePublisher.noteHit += EnemyProximityDetection;
+                notePublisher.noteNotHit += EnemyProximityDetection;
                 break;
             default:
                 break;
@@ -122,13 +143,17 @@ public class Enemy : MonoBehaviour
     {
         switch (movePattern)
         {
-            case MovePattern.SimpleMove:
-                notePublisher.noteHit -= EnemyMove;
-                notePublisher.noteNotHit -= EnemyMove;
+            case MovePattern.TowardsPlayer:
+                notePublisher.noteHit -= EnemyMoveTowardsPlayer;
+                notePublisher.noteNotHit -= EnemyMoveTowardsPlayer;
                 break;
-            case MovePattern.FastMove:
-                notePublisher.noteHit -= EnemyMove;
-                notePublisher.noteNotHit -= EnemyMove;
+            case MovePattern.RandomDirection:
+                notePublisher.noteHit -= EnemyRandomMovement;
+                notePublisher.noteNotHit -= EnemyRandomMovement;
+                break;
+            case MovePattern.ProximityDetection:
+                notePublisher.noteHit -= EnemyProximityDetection;
+                notePublisher.noteNotHit -= EnemyProximityDetection;
                 break;
             default:
                 break;
