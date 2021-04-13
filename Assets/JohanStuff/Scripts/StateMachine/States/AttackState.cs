a30feb0d0aaa984e1820adcbf80331c99f26e9e7
Gabinus <gabinus_93@hotmail.com>
2021-04-13 13:36:16 +0200

    StateMachine

---
+++ Assets/JohanStuff/Scripts/StateMachine/States/AttackState.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class AttackState : State
+{
+
+    public AttackState(Character character, StateMachine stateMachine) : base(character, stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+    }
+
+    public override void HandleInput()
+    {
+        base.HandleInput();
+
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+
+    }
+}
---
+++ Assets/JohanStuff/Scripts/StateMachine/States/ChargeAttackState.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class ChargeAttackState : State
+{
+
+    public ChargeAttackState(Character character, StateMachine stateMachine) : base(character, stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+    }
+
+    public override void HandleInput()
+    {
+        base.HandleInput();
+
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+
+    }
+}
