edf161fc45454f6751ca9ac7a950accd8ce05c9e
Melon332 <72919775+MehmetTanis-debug@users.noreply.github.com>
2021-04-15 10:26:53 +0200

    Fixed the animation for the floating text.

--- Assets/MehmetScripts/Scripts/FloatingTextScripts/FloatingText.cs
+++ Assets/MehmetScripts/Scripts/FloatingTextScripts/FloatingText.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class FloatingText : MonoBehaviour
 {
-    public float destroyTime = 2f;
+    public float destroyTime = 1.5f;
 
-    Vector3 offset = new Vector3(0, 0, 0);
+    Vector3 offset = new Vector3(0, 4, 0);
 
     // Start is called before the first frame update
     void Start()
