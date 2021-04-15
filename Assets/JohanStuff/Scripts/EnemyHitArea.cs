9e8da57a908c123ff88dddc914cec45160a6697d
Melon332 <72919775+MehmetTanis-debug@users.noreply.github.com>
2021-04-15 11:14:57 +0200

    Made damage more consistent

--- Assets/JohanStuff/Scripts/EnemyHitArea.cs
+++ Assets/JohanStuff/Scripts/EnemyHitArea.cs
@@ -7,13 +7,14 @@ public class EnemyHitArea : MonoBehaviour
     Enemy character;
     private void Awake()
     {
-        character = FindObjectOfType<Enemy>();
+        character = GetComponentInParent<Enemy>();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             character.playerIsInAttackArea = true;
+            Debug.Log("Player entered" + character.name);
         }
     }
     private void OnTriggerExit(Collider other)
@@ -21,6 +22,7 @@ public class EnemyHitArea : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             character.playerIsInAttackArea = false;
+            Debug.Log("Player exit" + character.name);
         }
     }
     private void OnEnable()
