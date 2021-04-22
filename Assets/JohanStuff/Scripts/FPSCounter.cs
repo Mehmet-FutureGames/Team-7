04cc079fdfa58a33df9032adde4a458f1408b427
Gabinus <gabinus_93@hotmail.com>
2021-04-22 17:51:25 +0200

    Fpscounter

--- Assets/JohanStuff/Scripts/FPSCounter.cs
+++ Assets/JohanStuff/Scripts/FPSCounter.cs
@@ -8,9 +8,14 @@ public class FPSCounter : MonoBehaviour
     private Text text;
 
     private float timer;
+    public float minVal;
+    int fps;
     void Start()
     {
+        
+        Application.targetFrameRate = 100;
         text = GetComponent<Text>();
+        StartCoroutine(UpdateMinVal());
     }
 
     // Update is called once per frame
@@ -18,10 +23,22 @@ public class FPSCounter : MonoBehaviour
     {
         if(Time.unscaledTime > timer)
         {
-            int fps = (int)(1f / Time.unscaledDeltaTime);
+            fps = (int)(1f / Time.unscaledDeltaTime);
             text.text = "FPS: " + fps.ToString();
             timer = Time.unscaledTime + 0.05f;
         }
-        
+    }
+
+    IEnumerator UpdateMinVal()
+    {
+        minVal = 1000;
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            if (minVal > fps)
+            {
+                minVal = fps;
+            }
+        }
     }
 }
