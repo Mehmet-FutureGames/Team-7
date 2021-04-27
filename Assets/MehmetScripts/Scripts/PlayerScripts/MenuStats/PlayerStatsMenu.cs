c03f3fb1826d6f4b1aea089d81b6d9374dbe3b3b
Melon332 <72919775+MehmetTanis-debug@users.noreply.github.com>
2021-04-27 09:25:51 +0200

    Update PlayerStatsMenu.cs

--- Assets/MehmetScripts/Scripts/PlayerScripts/MenuStats/PlayerStatsMenu.cs
+++ Assets/MehmetScripts/Scripts/PlayerScripts/MenuStats/PlayerStatsMenu.cs
@@ -82,6 +82,7 @@ public class PlayerStatsMenu : MonoBehaviour
                     characters[i].GetComponent<CharacterStats>().hasBeenBought = true;
                     notes -= pay;
                     notesText.text = notes.ToString();
+                    lockScreen.SetActive(false);
                     PlayerPrefs.SetInt("boughtCharacter" + currentCharacterSelected, characters[i].GetComponent<CharacterStats>().hasBeenBought ? 1 : 0);
                     Debug.Log(notes);
                 }
