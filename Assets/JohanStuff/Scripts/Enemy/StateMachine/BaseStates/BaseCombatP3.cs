48ff5461478a8db57d288a47636addf547087d87
Gabinus <gabinus_93@hotmail.com>
2021-05-05 13:27:44 +0200

    as

--- Assets/JohanStuff/Scripts/Enemy/StateMachine/BaseStates/BaseCombatP3.cs
+++ Assets/JohanStuff/Scripts/Enemy/StateMachine/BaseStates/BaseCombatP3.cs
@@ -12,20 +12,22 @@ public class BaseCombatP3 : State
     {
         Debug.Log("attack!");
         base.Enter();
+        hasAttacked = false;
         timer = 0.07f;
 
         enemy.gameObject.GetComponentInChildren<MeshRenderer>().material.color = Color.red;
 
     }
     float timer = 0.2f;
-
+    bool hasAttacked;
     public override void LogicUpdate()
     {
         base.LogicUpdate();
         timer -= Time.deltaTime;
-        if (timer <= 0)
+        if (timer <= 0 && hasAttacked == false)
         {
             enemy.EnemyAttack();
+            hasAttacked = true;
             enemy.area.SetActive(false);
         }
     }
