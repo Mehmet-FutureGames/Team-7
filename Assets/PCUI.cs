93ec9b1dc59079aaf8c18c142c513ab5a8855d5a
Gabinus <gabinus_93@hotmail.com>
2021-05-12 17:21:30 +0200

    PCUI

---
+++ Assets/PCUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCUI : MonoBehaviour
+{
+    private void Awake()
+    {
+#if UNITY_ANDROID
+    gameObject.SetActive(false);
+#endif
+    }
+
+}
