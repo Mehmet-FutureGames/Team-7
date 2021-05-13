5d9e7a06c4aa1698739715ab00c0fd35576d6b67
Melon332 <72919775+MehmetTanis-debug@users.noreply.github.com>
2021-05-13 16:15:19 +0200

    Fixed some menu bugs.
    Characters should now set their default values correctly.

--- Assets/MehmetScripts/Scripts/PlayerScripts/MenuStats/CharacterStats.cs
+++ Assets/MehmetScripts/Scripts/PlayerScripts/MenuStats/CharacterStats.cs
@@ -41,10 +41,6 @@ public class CharacterStats : MonoBehaviour
         hasBeenBought = PlayerPrefs.GetInt("boughtCharacter" + currentSelectedCharacter) == 1;
         hasStartedFirstTime = PlayerPrefs.GetInt("hasStartedFirstTime") == 1;
 
-        notesCostHealth = PlayerPrefs.GetInt("UpgradeHealth" + currentSelectedCharacter);
-        notesCostDamage = PlayerPrefs.GetInt("UpgradeDamage" + currentSelectedCharacter);
-        notesFrenzyCost = PlayerPrefs.GetInt("UpgradeFrenzy" + currentSelectedCharacter);
-
         gameObject.SetActive(false);
     }
     private void OnEnable()
