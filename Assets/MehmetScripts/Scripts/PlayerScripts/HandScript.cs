821ec29c74e93188d29cab33697e1d8b6dbaddb2
sirlaurents <emilsamuelssons@gmail.com>
2021-05-15 13:59:20 +0200

    Made some animations in character menu and shop

--- Assets/MehmetScripts/Scripts/PlayerScripts/HandScript.cs
+++ Assets/MehmetScripts/Scripts/PlayerScripts/HandScript.cs
@@ -8,7 +8,7 @@ public class HandScript : MonoBehaviour
     void Start()
     {
         var sword = Instantiate(GetComponentInParent<PlayerModels>().models[3], transform.position,Quaternion.identity, transform);
-        sword.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        sword.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
         sword.transform.localRotation = Quaternion.Euler(new Vector3(270, 0, 0));
         sword.transform.localPosition = new Vector3(0.01f, 0.005f, -0.003f);
     }
