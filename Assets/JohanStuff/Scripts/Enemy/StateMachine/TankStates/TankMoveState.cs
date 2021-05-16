1e347bc3f08a593def346be0fa1d920d61ccbe7f
Gabinus <gabinus_93@hotmail.com>
2021-05-16 15:34:59 +0200

    Update TankMoveState.cs

--- Assets/JohanStuff/Scripts/Enemy/StateMachine/TankStates/TankMoveState.cs
+++ Assets/JohanStuff/Scripts/Enemy/StateMachine/TankStates/TankMoveState.cs
@@ -14,7 +14,7 @@ public class TankMoveState : State
     }
     public override void LogicUpdate()
     {
-        enemy.animator.SetFloat("Blend", enemy.agent.velocity.magnitude);
+        
     }
 
     public override void Exit()
